348459ca83fb77fac41908e38b74d25db45616c6
Sarup <sarupbormon18@gmail.com>
2024-09-03 00:12:13 +0530

    Print with Name and University

---
+++ 03. C Program.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+
+    printf("Name:\tMr.Sarup Bormon\n"); //'\n' create a new line and'\t' create a tap
+    printf("University:\tUniversity of JNTUK");
+
+    return 0;
+}
