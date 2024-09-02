74ddc966e4b6e7fca674fc240c820efa620bcae1
Sarup <sarupbormon18@gmail.com>
2024-09-03 00:03:58 +0530

    Skeleton of a C program

---
+++ 01. C Program.c
@@ -0,0 +1,11 @@
+#include<stdio.h> //This is the header file that contains some standard functions and rule sets.
+int main()  //This main function which be called at first when the program will be executed
+{
+
+    /*This is the main body 
+    where all other code stuffs and logics will be written
+    */
+
+ return 0;  //For reason of return type,we need some integer to return.
+ 
+}
