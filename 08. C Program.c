d83314b09383bd50993491157ae11142cbf5c512
Sarup <sarupbormon18@gmail.com>
2024-09-13 00:24:49 +0530

    Avarage of three variable

---
+++ 08. C Program.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+     
+     double x,y,z,avg;
+     x = 10;
+     y = 20;
+     z = 30;
+     avg =(x+y+z)/3;
+     printf("Avarage of x,y and z is:%lf\n",avg);
+
+    return 0;
+}
