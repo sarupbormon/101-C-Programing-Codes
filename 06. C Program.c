606d8caff57c1315c1f9aa3dcf75dfc72e521992
Sarup <sarupbormon18@gmail.com>
2024-09-12 23:53:44 +0530

    Sum of three Variable

---
+++ 06. C Program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+     
+    int a,b,c,sum;
+      a =10;
+      b =20;
+      c =30;
+     sum = a+b+c;
+     printf("Sum of these three variable :%d\n",sum);
+
+
+    return 0;
+}
